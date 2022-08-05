fe8503cd3c778ad1fe9d4d66571da073d6f57f80
Сергей Шапарь <djoknakh@yandex.ru>
2022-08-05 16:03:20 +0300

    3

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,44 +5,49 @@ from .forms import NewsForm
 from django.views.generic import ListView
 
 
-class HomeNews()
-
 # Create your views here.
 def index(request):
     news = News.objects.all()
     categories = Category.objects.all()
     context = {
-        'news': news,
-        'title': 'Список новостей',
-        'categories': categories,
+        "news": news,
+        "title": "Список новостей",
+        "categories": categories,
     }
-    return render(request, template_name='news/index.html', context=context)
+    return render(request, template_name="news/index.html", context=context)
+
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news':news,
- 'categories': categories, 'category': category})
+    return render(
+        request,
+        "news/category.html",
+        {"news": news, "categories": categories, "category": category},
+    )
+
 
 def test(request):
-    return HttpResponse('<h1>тестовая страница</h1>')
+    return HttpResponse("<h1>тестовая страница</h1>")
+
 
 def view_news(request, news_id):
     news_item = News.objects.get(pk=news_id)
-    return render(request,'news/view_news.html',{'news_item':news_item})
+    return render(request, "news/view_news.html", {"news_item": news_item})
+
 
 def add_news(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = NewsForm(request.POST)
         if form.is_valid():
             # news = News.objects.create(**form.cleaned_data)
             news = form.save()
-            return redirect('home')
+            return redirect("home")
     else:
         form = NewsForm()
-    return render(request, 'news/add_news.html', {'form':form})
+    return render(request, "news/add_news.html", {"form": form})
+
 
 def link_page(request):
-    return render(request, 'news/link.html')
-
+    return render(request, "news/link.html")
